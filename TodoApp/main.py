d7719059f25859a24a76b077b9268c4dc9e42e81
HarshMistry <harshm@zignuts.com-at-075929790289>
2026-04-14 14:39:43 +0530

    - read todo funcation added for get specific todo by id.

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI,Depends,HTTPException,Path
 import models 
 from models import Todos
 from database import engine, SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
+from starlette import status
 
 app = FastAPI()
 
@@ -20,6 +21,14 @@ def get_db():
 db_dependancy = Annotated[Session,Depends(get_db)]
 
 
-@app.get("/")
+@app.get("/",status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependancy):
     return db.query(Todos).all()
+
+@app.get("/todo/{todo_id}",status_code=status.HTTP_200_OK)
+async def read_todo(db:db_dependancy,todo_id:int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    else:
+        raise HTTPException(status_code=404,detail='Todo not found.')
