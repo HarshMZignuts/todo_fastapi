bd83d06888adc58d26c669e3641546c7e92ee74a
HarshMistry <harshm@zignuts.com-at-075929790289>
2026-04-14 19:21:36 +0530

    - new token endpoint added. - authenticate user funcation added.

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -5,7 +5,8 @@ from sqlalchemy.orm import Session
 from starlette import status
 from create_user_request import CreateUserRequest
 from models import Users
-from passlib.context import CryptContext 
+from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
 
 
 router = APIRouter()
@@ -23,6 +24,14 @@ def get_db():
 
 db_dependancy = Annotated[Session,Depends(get_db)]
 
+def authenticate_user(user_name:str,password:str,db):
+    user = db.query(Users).filter(Users.user_name == user_name).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password,user.hashed_password):
+        return False
+    return True
+
 @router.post("/auth",status_code=status.HTTP_201_CREATED)
 async def create_user(db:db_dependancy,
                       create_user_request:CreateUserRequest):
@@ -38,3 +47,11 @@ async def create_user(db:db_dependancy,
 
     db.add(create_user_model)
     db.commit()
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,Depends()],
+                                 db: db_dependancy):
+    user = authenticate_user(form_data.username,form_data.password,db)
+    if not user:
+        return 'Failed Authentication'
+    return 'Successful Authentication'
