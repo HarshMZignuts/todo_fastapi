8bd27da0e36b8c34a2c144458a9d45792b3309c7
HarshMistry <harshm@zignuts.com-at-075929790289>
2026-04-15 14:56:55 +0530

    - user route added. - new end point added for get user detail & change password. - user verification request class added.

---
+++ TodoApp/routers/users.py
@@ -0,0 +1,55 @@
+from fastapi import Depends,HTTPException,Path,APIRouter
+from models import Todos,Users
+from database import SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
+from starlette import status
+from todo_request import TodoRequest
+from .auth import bcrypt_context, get_current_user
+from passlib.context import CryptContext
+from user_verification_request import UserVerification
+
+
+
+router = APIRouter(
+    prefix= '/user',
+    tags=['user']
+)
+ 
+
+def get_db():
+    db = SessionLocal() 
+    try:
+        yield db
+    finally:
+        db.close()
+# Depends is dependancy injection
+
+db_dependancy = Annotated[Session,Depends(get_db)]
+user_dependancy = Annotated[dict,Depends(get_current_user)]
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+@router.get("/", status_code=status.HTTP_200_OK)
+async def get_user(user: user_dependancy,db:db_dependancy):
+    if user is None:
+        raise HTTPException(status_code=401,detail='Authentication failed.')
+    
+    return db.query(Users).filter(Users.id == user.get('id')).first()
+
+@router.put("/password",status_code=status.HTTP_201_CREATED)
+async def change_password(user: user_dependancy,db:db_dependancy
+                          ,user_verification: UserVerification):
+    
+    if user is None:
+        raise HTTPException(status_code=401,detail='Authentication failed.')
+    
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+
+    if not bcrypt_context.verify(user_verification.password,user_model.hashed_password):
+        raise HTTPException(status_code=401,detail='Detail error on password change.')
+    
+    user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
+    db.add(user_model)
+    db.commit()
+    
+    
