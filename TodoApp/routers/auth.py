17efe178ba4b3bd51a62e9d9cac16180c5a33f5e
HarshMistry <harshm@zignuts.com-at-075929790289>
2026-04-15 10:57:01 +0530

    - jwt decode funcation added.

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta, timezone
 
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter,Depends,HTTPException
 from database import SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
@@ -8,8 +8,9 @@ from starlette import status
 from create_user_request import CreateUserRequest,Token
 from models import Users
 from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordRequestForm
-from jose import jwt
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt,JWTError
+
 
 
 
@@ -18,6 +19,7 @@ SECRET_KEY = 'aa81594dab604b072d6aa5afefc1d5c07b641a6490999d9407e66685d35750ad'
 ALGORITHM = 'HS256'
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
 
 
 def get_db():
@@ -44,6 +46,17 @@ def create_access_token(user_name:str,user_id:int,expires_delta:timedelta):
     expires = datetime.now(timezone.utc) + expires_delta
     encode.update({'exp':expires})
     return jwt.encode(encode,SECRET_KEY,algorithm=ALGORITHM)
+
+async def get_current_user(token: Annotated[str,Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        user_name: str = payload.get('sub')
+        user_id: str = payload.get('id')
+        if user_name is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail='Could not validate user.')
+        return {'user_name': user_name, 'id' : user_id}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail='Could not validate user.')
 
 
 @router.post("/auth",status_code=status.HTTP_201_CREATED)
